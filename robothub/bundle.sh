c0c8ef41931259174c47cd5eb83b5f45efc69265
Filip Prochazka <filip@thekingdom.nz>
2023-01-05 16:31:35 +0100

    fix arm build

--- robothub/bundle.sh
+++ robothub/bundle.sh
@@ -2,19 +2,13 @@
 
 set -e
 
-yarn gulp vscode-reh-web-linux-x64-min
-yarn gulp vscode-reh-web-linux-arm64-min
+yarn gulp vscode-reh-web-linux-${CODE_ARCH}-min
 
 START_DIR="$(pwd)"
 cd ..
 
-mv vscode-reh-web-linux-x64 robothub-studio-x86_64
-mv vscode-reh-web-linux-arm64 robothub-studio-aarch64
+mv vscode-reh-web-linux-${CODE_ARCH} robothub-studio-${LINUX_ARCH}
 
-tar cfJ robothub-studio-x86_64.tar.xz robothub-studio-x86_64
-rm -rf robothub-studio-x86_64
-mv robothub-studio-x86_64.tar.xz "${START_DIR}/"
-
-tar cfJ robothub-studio-aarch64.tar.xz robothub-studio-aarch64
-rm -rf robothub-studio-aarch64
-mv robothub-studio-aarch64.tar.xz "${START_DIR}/"
+tar cfJ robothub-studio-${LINUX_ARCH}.tar.xz robothub-studio-${LINUX_ARCH}
+rm -rf robothub-studio-${LINUX_ARCH}
+mv robothub-studio-${LINUX_ARCH}.tar.xz "${START_DIR}/"
